cf34ab7a72e76b2f2b7fdc4e411abcac7df0433b
Arnel Enero <aenero@gmail.com>
2021-03-14 21:09:16 +0800

    Refactor plugin support

--- src/plugin.js
+++ src/plugin.js
@@ -1,13 +1,12 @@
 export const plugins = []
 
-export const plugin = (pluginPkg, options = {}) => {
+export const plugin = (pluginFn, options = {}) => {
   if (typeof options !== 'object')
     throw new Error('Plug-in options should be an object.')
 
-  if (typeof pluginPkg !== 'function')
-    throw new Error('Invalid plug-in package')
+  if (typeof pluginFn !== 'function') throw new Error('Invalid plug-in package')
 
-  const pluginObj = pluginPkg(options)
+  const pluginObj = pluginFn(options)
 
   if (typeof pluginObj !== 'object') throw new Error('Invalid plug-in')
 
