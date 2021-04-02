0224b8c4f3525d34716aa8bde66ef3e1f5dd7459
Arnel Enero <aenero@gmail.com>
2021-04-02 21:59:33 +0800

    Update package

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,22 @@
-const { NODE_ENV, BABEL_ENV } = process.env;
-const commonjs = NODE_ENV === 'test' || BABEL_ENV === 'commonjs';
-const loose = true;
+const { NODE_ENV, BABEL_ENV } = process.env
+const commonjs = NODE_ENV === 'test' || BABEL_ENV === 'commonjs'
+const loose = true
 
 module.exports = {
-  presets: [['@babel/env', { loose, modules: false }], '@babel/react'],
+  presets: [
+    [
+      '@babel/env',
+      {
+        loose,
+        modules: false,
+        targets: commonjs ? { ie: 11 } : { node: 'current' }
+      }
+    ],
+    '@babel/react'
+  ],
   plugins: [
     ['@babel/proposal-object-rest-spread', { loose }],
-    ['@babel/proposal-class-properties', { loose }],
     commonjs && ['@babel/transform-modules-commonjs', { loose }],
-    ['@babel/transform-runtime', { useESModules: !commonjs }],
-  ].filter(Boolean),
-};
+    ['@babel/transform-runtime', { useESModules: !commonjs }]
+  ].filter(Boolean)
+}
