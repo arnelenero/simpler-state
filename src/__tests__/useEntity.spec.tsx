36a28e55635edf3732f5c913ddd36794702acea0
Arnel Enero <aenero@gmail.com>
2023-05-07 13:54:56 +0800

    Update tests

--- src/__tests__/useEntity.spec.tsx
+++ src/__tests__/useEntity.spec.tsx
@@ -2,209 +2,205 @@
 import { TextEncoder } from 'util'
 global.TextEncoder = TextEncoder
 
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import ReactDOMServer from 'react-dom/server'
-import { act } from 'react-dom/test-utils'
-import { render } from '@testing-library/react'
+import { act, render, renderHook } from '@testing-library/react'
 
 import { useEntity } from '../useEntity'
 import { entity } from '../entity'
 import { shallowEqual } from '../equality'
 
-import type { Entity } from '../entity'
-
-type EqTest<T = any> = (a: T, b: T) => boolean
-
 describe('useEntity', () => {
-  const counterView = (selectKey?: string, equalityFn?: EqTest) => {
-    return () => {
-      const [key, setKey] = useState(selectKey)
-      const selector = key
-        ? equalityFn === shallowEqual
-          ? (val: any) => {
-              return { [key]: val[key] }
-            }
-          : (val: any) => val[key]
-        : undefined
-      setSelectKey = setKey // Allows modifying the selector key from outside
-
-      hookValue = useEntity(counter, selector, equalityFn)
-
-      useEffect(() => {
-        renderCount++
-      })
-
-      return <></>
-    }
+  interface Counter {
+    value: number
+    lastUpdated: Date
+  }
+  interface HookOptions<T> {
+    transform?: (value: Counter) => T
+    equality?: (a: T, b: T) => boolean
   }
 
-  const mountCounter = (selectKey?: string, equalityFn?: EqTest) => {
-    const CounterView = counterView(selectKey, equalityFn)
-    component = render(<CounterView />)
+  const counter = entity<Counter>({ value: 0, lastUpdated: new Date() })
+
+  /** Action that increments `counter.value`. */
+  function increment(by = 1) {
+    counter.set({
+      value: counter.get().value + by,
+      lastUpdated: new Date(),
+    })
   }
 
-  const mountCounterWithAnother = (CounterB: React.FC) => {
-    const CounterView = counterView()
-    component = render(
-      <div>
-        <CounterView />
-        <CounterB />
-      </div>,
+  /** Action that updates `counter.lastUpdated` to current timestamp. */
+  function touch() {
+    counter.set({ ...counter.get(), lastUpdated: new Date() })
+  }
+
+  /** Use this when you need to inspect the hook value. */
+  function renderUseEntity<T = Counter>(options: HookOptions<T> = {}) {
+    return renderHook(
+      ({ transform, equality }: HookOptions<T>) =>
+        useEntity(counter, transform, equality),
+      { initialProps: options },
     )
   }
 
-  let counter: Entity
-  let increment: Function
-  let touch: Function
-  let component: ReturnType<typeof render>
-  let renderCount = 0
-  let hookValue: any
-  let setSelectKey: Function
-
-  beforeEach(() => {
-    counter = entity({ value: 0, lastUpdated: new Date() })
-    increment = (by = 1) => {
-      counter.set({
-        value: counter.get().value + by,
-        lastUpdated: new Date(),
-      })
-    }
-    touch = () => counter.set({ ...counter.get(), lastUpdated: new Date() })
-  })
-
-  afterEach(() => {
-    component.unmount()
-  })
-
-  it('returns the current value of the entity', () => {
-    mountCounter()
-
-    expect(hookValue).toBeInstanceOf(Object)
-    expect(hookValue).toHaveProperty('value', 0)
-  })
-
-  it('re-renders the component on each change in entity value caused by an action', () => {
-    mountCounter()
-
-    const prevRenderCount = renderCount
-    act(() => {
-      increment()
-    })
-    expect(renderCount).toBe(prevRenderCount + 1)
-  })
-
-  it('supports server-side rendering', () => {
-    // Suppress the annoying useLayoutEffect error message
-    const origConsoleError = console.error
-    console.error = jest.fn()
-
-    const CounterView = counterView()
-    expect(() => {
-      ReactDOMServer.renderToString(<CounterView />)
-    }).not.toThrow()
-
-    console.error = origConsoleError
-  })
-
-  it('always provides the updated entity value to the component', () => {
-    mountCounter()
-
-    act(() => {
-      increment()
-    })
-    expect(hookValue).toHaveProperty('value', 1)
-  })
-
-  it('subscribes the component to changes in only the relevant fields when using a transform', () => {
-    mountCounter('value')
-
-    const prevRenderCount = renderCount
-    act(() => {
-      touch()
-    })
-    expect(renderCount).toBe(prevRenderCount)
-  })
-
-  it('applies the transform (if any) to the entity value provided to the component', () => {
-    mountCounter('value')
-
-    act(() => {
-      increment()
-    })
-    expect(hookValue).toBe(1)
-  })
-
-  it('updates subscription whenever the transform changes', () => {
-    mountCounter('value')
-
-    act(() => {
-      increment()
-    })
-    expect(hookValue).toBe(1)
-
-    act(() => {
-      setSelectKey('lastUpdated')
-    })
-    // hook now uses a different selector in the transform
-    expect(hookValue).toBeInstanceOf(Date)
-  })
-
-  it('supports custom equality function for subcription when using a transform', () => {
-    mountCounter('value', shallowEqual)
-
-    const prevRenderCount = renderCount
-    act(() => {
-      touch()
-    })
-    expect(renderCount).toBe(prevRenderCount)
-  })
-
-  it('subscribes all components that use the hook', () => {
-    let renderCountB = 0
-    let hookValueB = null
-    const CounterB = () => {
-      hookValueB = useEntity(counter)
+  /** Use this when you need to inspect the render count. */
+  function renderComponent<T = Counter>({
+    transform,
+    equality,
+  }: HookOptions<T> = {}) {
+    const renderCount = { current: 0 }
+    function CounterView() {
+      const value = useEntity(counter, transform, equality)
+      // Increment `renderCount` on every render.
       useEffect(() => {
-        renderCountB++
+        renderCount.current++
       })
       return <></>
     }
 
-    mountCounterWithAnother(CounterB)
+    const rendered = render(<CounterView />)
+    return { ...rendered, renderCount }
+  }
 
-    const prevRenderCount = renderCount
-    const prevRenderCountB = renderCountB
+  beforeEach(() => {
+    counter.init()
+  })
+
+  it('returns the current value of the entity', () => {
+    const { result } = renderUseEntity()
+
+    expect(result.current).toBeInstanceOf(Object)
+    expect(result.current).toHaveProperty('value', 0)
+  })
+
+  it('re-renders the component on each change in entity value caused by an action', () => {
+    const { renderCount } = renderComponent()
     act(() => {
       increment()
     })
-    expect(renderCount).toBe(prevRenderCount + 1)
-    expect(hookValue).toHaveProperty('value', 1)
-    expect(renderCountB).toBe(prevRenderCountB + 1)
-    expect(hookValueB).toHaveProperty('value', 1)
+
+    expect(renderCount.current).toBe(2)
+  })
+
+  it('supports server-side rendering', () => {
+    function CounterView() {
+      const { value } = useEntity(counter)
+      return <>{value}</>
+    }
+
+    expect(() => {
+      ReactDOMServer.renderToString(<CounterView />)
+    }).not.toThrow()
+  })
+
+  it('always provides the updated entity value to the component', () => {
+    const { result } = renderUseEntity()
+    act(() => {
+      increment()
+    })
+
+    expect(result.current).toHaveProperty('value', 1)
+  })
+
+  it('subscribes the component to changes in only the relevant fields when using a transform', () => {
+    const { renderCount } = renderComponent({
+      transform: counter => counter.value,
+    })
+    act(() => {
+      touch()
+    })
+
+    expect(renderCount.current).toBe(1)
+  })
+
+  it('applies the transform (if any) to the entity value provided to the component', () => {
+    const { result } = renderUseEntity({
+      transform: counter => counter.value,
+    })
+    act(() => {
+      increment()
+    })
+
+    expect(result.current).toBe(1)
+  })
+
+  it('updates subscription whenever the transform changes', () => {
+    const { result, rerender } = renderUseEntity<number | Date>({
+      transform: counter => counter.value,
+    })
+    act(() => {
+      increment()
+    })
+
+    expect(result.current).toBe(1)
+
+    rerender({ transform: counter => counter.lastUpdated })
+
+    // hook now uses a different selector in the transform
+    expect(result.current).toBeInstanceOf(Date)
+  })
+
+  it('uses strict equality (identity) by default when determining updates', () => {
+    const { renderCount } = renderComponent({
+      // This transform results in a fresh object every time.
+      transform: ({ value }) => ({ value }),
+    })
+    act(() => {
+      // This action does not change the `value` property.
+      touch()
+    })
+
+    expect(renderCount.current).toBe(2)
+  })
+
+  it('supports custom equality function for subcription when using a transform', () => {
+    const { renderCount } = renderComponent({
+      // This transform results in a fresh object every time.
+      transform: ({ value }) => ({ value }),
+      equality: shallowEqual,
+    })
+    act(() => {
+      // This action does not change the `value` property.
+      touch()
+    })
+
+    expect(renderCount.current).toBe(1)
+  })
+
+  it('subscribes all components that use the hook', () => {
+    const { result: resultA } = renderUseEntity({
+      transform: counter => counter.value,
+    })
+    const { result: resultB } = renderUseEntity({
+      transform: counter => counter.value,
+    })
+    act(() => {
+      increment()
+    })
+
+    expect(resultA.current).toBe(1)
+    expect(resultB.current).toBe(1)
   })
 
   it('unsubscribes the component from the entity when it unmounts', () => {
-    mountCounter()
-
-    const prevRenderCount = renderCount
-    component.unmount()
+    const { renderCount, unmount } = renderComponent()
+    unmount()
     act(() => {
       increment()
     })
-    expect(renderCount).toBe(prevRenderCount)
+
+    // This is still the original render count. Unmount does not reset it to 0.
+    expect(renderCount.current).toBe(1)
   })
 
   it('checks for valid entity', () => {
     const origConsoleError = console.error
     console.error = jest.fn()
 
-    const CounterView = () => {
-      // @ts-ignore
-      const { value } = useEntity({ value: 0 })
-      return <>{value}</>
-    }
     expect(() => {
-      component = render(<CounterView />)
+      // @ts-ignore: This simulates wrong usage in JS (no TS checking).
+      renderHook(() => useEntity({ value: 0 }))
     }).toThrow()
 
     console.error = origConsoleError
