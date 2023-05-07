310408addd7fb0ea78a06ad31804b323a13f738f
Arnel Enero <aenero@gmail.com>
2023-05-07 15:09:05 +0800

    Update tests

--- src/tools/__tests__/resetAll.spec.tsx
+++ src/tools/__tests__/resetAll.spec.tsx
@@ -1,80 +1,24 @@
-import React, { useEffect } from 'react'
-import { act } from 'react-dom/test-utils'
-import { render } from '@testing-library/react'
-
-import resetAll from '../resetAll'
+import { resetAll } from '../resetAll'
 import { enableStore } from '../store'
 import { entity } from '../../entity'
-import { useEntity } from '../../useEntity'
 
 import type { Entity } from '../../entity'
 
 describe('resetAll', () => {
-  const TestShell = () => {
-    useEffect(() => {
-      return () => resetAll()
-    })
-    return <CounterView />
-  }
-
-  const CounterView = () => {
-    hookValueA = useEntity(counter)
-    hookValueB = useEntity(countdown)
-
-    useEffect(() => {
-      mountCount++
-    }, [])
-
-    return null
-  }
-
-  let counter: Entity
-  let increment: Function
-  let countdown: Entity
-  let decrement: Function
-  let component: ReturnType<typeof render>
-  let hookValueA: any
-  let hookValueB: any
-  let mountCount = 0
-
-  enableStore()
-
-  beforeEach(() => {
-    counter = entity(0)
-    increment = (by = 1) => counter.set(counter.get() + by)
-
-    countdown = entity(10)
-    decrement = (by = 1) => countdown.set(countdown.get() - by)
-  })
-
-  afterEach(() => {
-    component.unmount()
-  })
-
   it('resets all entities to initial value', () => {
-    component = render(<TestShell />)
+    enableStore()
 
-    const prevMountCount = mountCount
+    const counters: Entity<number>[] = []
+    for (let i = 0; i < 5; i++) {
+      counters[i] = entity(0)
+    }
+    counters.forEach(counter => counter.set(1))
 
-    expect(hookValueA).toBe(0)
-    act(() => {
-      increment()
+    resetAll()
+
+    counters.forEach(counter => {
+      expect(counter.get()).toBe(0)
     })
-    expect(hookValueA).toBe(1)
-
-    expect(hookValueB).toBe(10)
-    act(() => {
-      decrement()
-    })
-    expect(hookValueB).toBe(9)
-
-    component.unmount()
-
-    component = render(<TestShell />)
-
-    expect(mountCount).toBe(prevMountCount + 1)
-    expect(hookValueA).toBe(0)
-    expect(hookValueB).toBe(10)
   })
 
   it('requires store to be enabled and throws error otherwise', () => {
